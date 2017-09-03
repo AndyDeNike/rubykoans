29c0e2030f7e622b2fc352fd531f93610e5a1ebf
Andrew DeNike <denikeandrew@gmail.com>
2017-09-02 23:50:53 -0700

    Progression

--- about_constants.rb
+++ about_constants.rb
@@ -35,7 +35,7 @@ class AboutConstants < Neo::Koan
   end
 
   def test_nested_classes_inherit_constants_from_enclosing_classes
-    assert_equal __, Animal::NestedAnimal.new.legs_in_nested_animal
+    assert_equal 4, Animal::NestedAnimal.new.legs_in_nested_animal
   end
 
   # ------------------------------------------------------------------
@@ -47,7 +47,7 @@ class AboutConstants < Neo::Koan
   end
 
   def test_subclasses_inherit_constants_from_parent_classes
-    assert_equal __, Reptile.new.legs_in_reptile
+    assert_equal 4, Reptile.new.legs_in_reptile
   end
 
   # ------------------------------------------------------------------
@@ -63,11 +63,13 @@ class AboutConstants < Neo::Koan
   end
 
   def test_who_wins_with_both_nested_and_inherited_constants
-    assert_equal __, MyAnimals::Bird.new.legs_in_bird
+    assert_equal 2, MyAnimals::Bird.new.legs_in_bird
   end
 
   # QUESTION: Which has precedence: The constant in the lexical scope,
-  # or the constant from the inheritance hierarchy?
+  # or the constant from the inheritance hierarchy?  
+  # ANSWER: The constant containing the nested class has precedence 
+  # over the inheited class 
 
   # ------------------------------------------------------------------
 
@@ -78,10 +80,13 @@ class AboutConstants < Neo::Koan
   end
 
   def test_who_wins_with_explicit_scoping_on_class_definition
-    assert_equal __, MyAnimals::Oyster.new.legs_in_oyster
+    assert_equal 4, MyAnimals::Oyster.new.legs_in_oyster
   end
 
   # QUESTION: Now which has precedence: The constant in the lexical
   # scope, or the constant from the inheritance hierarchy?  Why is it
   # different than the previous answer?
+  # ANSWER: When defining MyAnimals::Oyster, you are in global scope 
+  # therefore Oyster does not take on the internal constants of MyAnimals
+  # rather it lastly inhertis Animal constant LEGS = 4 
 end
